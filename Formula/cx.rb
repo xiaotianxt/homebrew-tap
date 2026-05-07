d92eedb5153e58a991f39e1c59747c8ef580b3c3
xiaotianxt <me@xiaotian.dev>
2026-05-07 16:16:59 -0700

    cx 0.2.30

--- Formula/cx.rb
+++ Formula/cx.rb
@@ -1,10 +1,10 @@
 class Cx < Formula
   desc "Fast local Codex launcher, stdin wrapper, and slot manager"
   homepage "https://github.com/xiaotianxt/cx"
-  url "https://github.com/xiaotianxt/cx/releases/download/v0.2.29/cx-v0.2.29-darwin-arm64.tar.gz"
-  sha256 "077f5dc087d4d8c5d25798fea432f8952af724e271cbfc27272e9cb2a476ede0"
+  url "https://github.com/xiaotianxt/cx/releases/download/v0.2.30/cx-v0.2.30-darwin-arm64.tar.gz"
+  sha256 "b09714d00e78f710063c70bf313647cfe456a861719a609996327768d11b0dcf"
   license "MIT"
-  version "0.2.29"
+  version "0.2.30"
 
   depends_on arch: :arm64
 

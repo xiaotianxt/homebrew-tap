0bc799f37d627f66e18478107be8ca9eb598ba5e
xiaotianxt <me@xiaotian.dev>
2026-05-16 09:44:57 -0700

    rust-silk 0.1.3

--- Formula/rust-silk.rb
+++ Formula/rust-silk.rb
@@ -1,20 +1,35 @@
 class RustSilk < Formula
   desc "SILK v3 encoder and decoder CLI"
   homepage "https://github.com/wangnov/rust-silk"
-  url "https://crates.io/api/v1/crates/rust-silk/0.1.0/download"
-  sha256 "ff872e7d5808b092b18d8a696f4cd45717d03fefeac498d535aca84461184559"
+  version "0.1.3"
   license "BSD-3-Clause"
 
-  bottle do
-    root_url "https://github.com/xiaotianxt/homebrew-tap/releases/download/rust-silk-0.1.0"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce486d15c7d52835ea489794d3b42c77513b7153c37862a81413f67b24a51328"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/wangnov/rust-silk/releases/download/v0.1.3/rust-silk-aarch64-apple-darwin.tar.xz"
+      sha256 "cf2ce5e3d4803c10c92af355577ee0530ab9f08bf4eb24449febbb6ac6139b00"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/wangnov/rust-silk/releases/download/v0.1.3/rust-silk-x86_64-apple-darwin.tar.xz"
+      sha256 "1237ba03af81ff26e23e82bdbd56dba8ba17d5eff00fe75fdbb74e617e059111"
+    else
+      odie "unsupported macOS architecture"
+    end
   end
 
-  depends_on "rust" => :build
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/wangnov/rust-silk/releases/download/v0.1.3/rust-silk-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "5dd608cb7409a0e0599690df24dab5193e6b4c8e2a99ffcf50be35cda1b117d1"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/wangnov/rust-silk/releases/download/v0.1.3/rust-silk-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "d27abce91e7fc885e5e139803daf6f9aa0412f2d8610775d55d0196c392fdb02"
+    else
+      odie "unsupported Linux architecture"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "rust-silk"
   end
 
   test do

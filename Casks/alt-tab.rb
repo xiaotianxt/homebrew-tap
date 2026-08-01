b00f43a9e4cc0bab29c029907f5702903d71eaca
Andy Tian <me@xiaotian.dev>
2026-07-31 22:30:58 -0400

    fix: use Homebrew-friendly AltTab archive

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -2,7 +2,7 @@ cask "alt-tab" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/xiaotianxt/alt-tab-macos/releases/latest/download/AltTab-local-pro.zip",
+  url "https://github.com/xiaotianxt/alt-tab-macos/releases/latest/download/AltTab-local-pro.tar.xz",
       verified: "github.com/xiaotianxt/alt-tab-macos/"
   name "AltTab Local Pro"
   desc "Automatically rebased local testing build of AltTab"

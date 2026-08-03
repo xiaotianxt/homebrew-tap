091d8497592e00d283ab8a8a6c843bc65fb67b26
xiaotianxt <me@xiaotian.dev>
2026-08-02 20:01:55 -0400

    tg 2.2.1

--- Formula/tg.rb
+++ Formula/tg.rb
@@ -1,13 +1,13 @@
 class Tg < Formula
   desc "本地 Telegram 桌面聊天记录读取 CLI 工具"
   homepage "https://github.com/xiaotianxt/tg"
-  version "2.2.0"
+  version "2.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/xiaotianxt/tg/releases/download/v2.2.0/tg-v2.2.0-darwin-arm64.tar.gz"
-      sha256 "c1dd6eb8b131cceafba0b021104015c779a543f7509fa07036859fea1c6073ab"
+      url "https://github.com/xiaotianxt/tg/releases/download/v2.2.1/tg-v2.2.1-darwin-arm64.tar.gz"
+      sha256 "d1ac861383ab1f6c7fd1fad349dc7d5ac3efb62c718190d90ec86f1fbdc4bbc8"
     else
       odie "tg provides prebuilt macOS releases for Apple Silicon only"
     end
@@ -15,11 +15,11 @@ class Tg < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/xiaotianxt/tg/releases/download/v2.2.0/tg-v2.2.0-linux-arm64.tar.gz"
-      sha256 "032e35c34e672d305f4141c3c000a7da366b9da4c738d7c07be44814d88c0202"
+      url "https://github.com/xiaotianxt/tg/releases/download/v2.2.1/tg-v2.2.1-linux-arm64.tar.gz"
+      sha256 "6363f3349c8886a03c4e5b40222fae2cda801645f2a1f3890046452aa621c6dc"
     elsif Hardware::CPU.intel?
-      url "https://github.com/xiaotianxt/tg/releases/download/v2.2.0/tg-v2.2.0-linux-x86_64.tar.gz"
-      sha256 "e28b764230e72381612dace17984f33e911eff80b9143cbb8cc81b2dd3d88a1a"
+      url "https://github.com/xiaotianxt/tg/releases/download/v2.2.1/tg-v2.2.1-linux-x86_64.tar.gz"
+      sha256 "5b7baf982537349665642e0510419294c850e62be3ba5c3f89e42266a61a3b9c"
     else
       odie "unsupported Linux architecture"
     end
@@ -29,14 +29,14 @@ class Tg < Formula
 
   def caveats
     <<~EOS
-      `tg keys --method lldb-login` uses Apple's lldb/debugserver while you log out and back in once.
-      Install Apple Command Line Tools when that mode is needed:
-        xcode-select --install
+      `tg keys --method login` uses LLDB on macOS or GDB on Linux while you log out and back in once.
+      Install Apple Command Line Tools on macOS, or GDB on Linux, when that mode is needed.
     EOS
   end
 
   def install
     bin.install "tg"
+    doc.install "LICENSE", "THIRD_PARTY_LICENSES"
     generate_completions_from_executable(bin/"tg", "completions")
   end
 
